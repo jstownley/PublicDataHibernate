695c1dfff8fa2f09434b2bb00a73f0b842fee8ca
jonathan townley <jstownley@gmail.com>
2016-09-14 19:29:16 -0500

    Created DB class
    This update includes:
      - Completed Country class, including Builder Pattern CountryBuilder
    inline class
      - Main application SessionFactory, StandardRegistery, and empty PSVM
    function

--- src/main/java/com/teamtreehouse/PublicData/Application.java
+++ src/main/java/com/teamtreehouse/PublicData/Application.java
@@ -1,4 +1,22 @@
 package com.teamtreehouse.PublicData;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.service.ServiceRegistry;
+
 public class Application {
+    // Hold a reusable reference to a Session Factory (since we need only one)
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory() {
+        // Create a StandardServiceRegistery
+        final ServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+        return new MetadataSources(registry).buildMetadata().buildSessionFactory();
+    }
+
+    public static void main(String[] args) {
+
+    }
+
 }
--- src/main/java/com/teamtreehouse/PublicData/model/Country.java
+++ src/main/java/com/teamtreehouse/PublicData/model/Country.java
@@ -1,4 +1,99 @@
 package com.teamtreehouse.PublicData.model;
 
+import javax.persistence.*;
+
+@Entity
 public class Country {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column
+    private String code;
+
+    @Column
+    private String name;
+
+    @Column
+    private float internetUsers;
+
+    @Column
+    private float adultLiteracyRate;
+
+    // Default constructor for JPA
+    public Country() {}
+
+    public Country(CountryBuilder builder) {
+        this.code = builder.code;
+        this.name = builder.name;
+        this.internetUsers = builder.internetUsers;
+        this.adultLiteracyRate = builder.adultLiteracyRate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getInternetUsers() {
+        return internetUsers;
+    }
+
+    public void setInternetUsers(float internetUsers) {
+        this.internetUsers = internetUsers;
+    }
+
+    public float getAdultLiteracyRate() {
+        return adultLiteracyRate;
+    }
+
+    public void setAdultLiteracyRate(float adultLiteracyRate) {
+        this.adultLiteracyRate = adultLiteracyRate;
+    }
+
+    public static class CountryBuilder {
+        private String code;
+        private String name;
+        private float internetUsers;
+        private float adultLiteracyRate;
+
+        public CountryBuilder(String name, String code) {
+            this.name = name;
+            this.code = code;
+        }
+
+        public CountryBuilder withInternetUsers(float internetUsers) {
+            this.internetUsers = internetUsers;
+            return this;
+        }
+
+        public CountryBuilder withAdultLiteracyRate(float adultLiteracyRate) {
+            this.adultLiteracyRate = adultLiteracyRate;
+            return this;
+        }
+
+        public Country build() {
+            return new Country(this);
+        }
+    }
 }
